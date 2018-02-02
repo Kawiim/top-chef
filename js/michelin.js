a895d86158886380681e5e68fb8b2875a9824d29
Karim Baali <baali.karim95@gmail.com>
2018-02-02 17:11:25 +0100

    Scrape anme of resto, only first city for now

--- js/michelin.js
+++ js/michelin.js
@@ -9,10 +9,20 @@ module.exports = {
 	        if(!error){
 
 	            var $ = cheerio.load(html);
-	            var listRest;
 
-                $('.field__item a').each(function(i, elm) {
-				    console.log($(this).text()) 
+                $('.content .field .field__items .field__item a').each(function(i, elm) {
+				    var url_lvl2 = $(this).attr('href')
+				    request(url_lvl2, function(error, response, html){
+				    	if(!error){
+				    		var $_2 = cheerio.load(html);
+
+				    		$_2('.poi-search-result li .node--poi-card').each(function(i, elm) {
+				    			var title = $_2(this).attr('attr-gtm-title')
+				    			console.log(title)
+
+				    		})
+				    	}
+				    })
 				});
 
                 callback("listRest");
