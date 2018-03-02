63ddf1355e26d2ac07d50130d8f9f57961f1c65f
Karim Baali <baali.karim95@gmail.com>
2018-03-02 17:47:46 +0100

    Displaying deals for starred restaurand

--- src/Deals.js
+++ src/Deals.js
@@ -1,13 +1,44 @@
 import React, { Component } from "react";
  
+ const apiUrl = "http://localhost:6969/deals"
+
 class Deals extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      deals: [],
+    };
+  }
+
+  componentDidMount() {
+    fetch(apiUrl, { accept: 'application/json' })
+      .then(response => response.json())
+      .then(data => this.setState({ deals: data }));
+  }
+
   render() {
+    const { deals } = this.state;
     return (
       <div>
         <h2>Deals</h2>
-        <p>The easiest thing to do is post on
-        our <a href="http://forum.kirupa.com">forums</a>.
-        </p>
+        <p>The latest deals on starred restaurants in France, just for you !</p>
+
+        <ul>
+          {deals.map(rest =>
+            <li key={rest._id}>
+              <h3>{rest.name}</h3>
+              <ul>
+              {rest.deals.map(deal =>
+                <li key={deal.id}>
+                  <h4> {deal.title} </h4>
+                  <p> {deal.exclusions} </p>
+                </li>
+              )}
+              </ul>
+            </li>
+          )}
+        </ul>
       </div>
     );
   }
